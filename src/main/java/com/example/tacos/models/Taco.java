e9d0afb005495bd19d7b65bd730bdf50b7d8daad
Md. Abdus Salam <salaam.mbstu@gmail.com>
2024-08-19 09:14:39 +0600

    feat: add controller, models, and lombok

---
+++ src/main/java/com/example/tacos/models/Taco.java
@@ -0,0 +1,14 @@
+package com.example.tacos.models;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Taco {
+
+    private String name;
+
+    private List<Ingredient> ingredients;
+
+}
